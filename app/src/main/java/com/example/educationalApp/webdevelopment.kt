83cda808ac141842ae8b34e66cc61e3b3bc08d4b
sangyan11 <sangyanbhagi@gmail.com>
2021-03-15 22:43:53 +0530

    Webdevadded

--- app/src/main/java/com/example/educationalApp/webdevelopment.kt
+++ app/src/main/java/com/example/educationalApp/webdevelopment.kt
@@ -16,9 +16,64 @@ class webdevelopment : AppCompatActivity() {
 
     }
 
+    fun PaidCourse(view: View) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.udemy.com/course/the-complete-web-development-bootcamp"))
+        startActivity(browserIntent)
+    }
+
+    fun NodeJs(view: View) {
+        val btn =findViewById<Button>(R.id.roadmap_btn2)
+        val videoId = "umzJU6OzjNI"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$videoId"))
+        intent.putExtra("VIDEO_ID", videoId)
+        startActivity(intent)
+    }
+
+    fun ProjectIdeas(view: View) {
+        val btn =findViewById<Button>(R.id.roadmap_btn2)
+        val videoId = "SI5ISZa0IL0"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$videoId"))
+        intent.putExtra("VIDEO_ID", videoId)
+        startActivity(intent)
+
+    }
+
+    fun FreeCourse(view: View) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://digitaldefynd.com/best-free-web-development-courses-tutorials-certification/"))
+        startActivity(browserIntent)
+    }
+
     fun Roadmap(view: View) {
-        val btn =findViewById<Button>(R.id.roadmap_btn)
+        val btn =findViewById<Button>(R.id.roadmap_btn2)
         val videoId = "GLk7-imcjiI"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$videoId"))
+        intent.putExtra("VIDEO_ID", videoId)
+        startActivity(intent)
+
+    }
+
+    fun Roadmap2(view: View) {
+        val btn =findViewById<Button>(R.id.roadmap_btn2)
+        val videoId = "nknwAOtmtDk"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$videoId"))
+        intent.putExtra("VIDEO_ID", videoId)
+        startActivity(intent)
+    }
+
+    fun YtPlaylist2(view: View) {
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=6mbwJ2xhgzM&list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg"))
+        startActivity(browserIntent)
+
+    }
+    fun YtPlayList1(view: View) {
+
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=l1EssrLxt7E&list=PLfqMhTWNBTe3H6c9OGXb5_6wcc1Mca52n"))
+        startActivity(browserIntent)
+    }
+
+    fun YtPlaylist3(view: View) {
+        val btn =findViewById<Button>(R.id.roadmap_btn2)
+        val videoId = "Q33KBiDriJY"
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$videoId"))
         intent.putExtra("VIDEO_ID", videoId)
         startActivity(intent)
